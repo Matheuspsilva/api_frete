486677921af803e120d67d82ae5f78e927892282
Matheus <matheus.silva@jn2.com.br>
2021-10-02 18:18:27 -0300

    Add FreteRequest

--- app/Http/Controllers/Api/FreteController.php
+++ app/Http/Controllers/Api/FreteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\FreteRequest;
 use App\Models\Frete;
 use App\Models\Veiculo;
 use Illuminate\Http\Request;
@@ -35,7 +36,7 @@ class FreteController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(FreteRequest $request)
     {
         try {
             $data = $request->all();
@@ -69,7 +70,7 @@ class FreteController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(FreteRequest $request, $id)
     {
         $data = $request->all();
 
